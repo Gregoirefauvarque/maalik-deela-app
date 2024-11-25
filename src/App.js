78fa4a5f6cb9998fbbecc29dcafd20e923a21c11
Gregoirefauvarque <gregoirefauvarque@hotmail.be>
2024-11-25 19:14:33 +0100

    Add App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MathGame from './MathGame';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-blue-50 to-purple-50 p-4">
+      <MathGame />
+    </div>
+  );
+}
+
+export default App;
